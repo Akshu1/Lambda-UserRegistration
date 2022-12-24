95bf993b04e72972dd22c27e58c0c8ccf3eaba52
Akshatha <49878763+Akshu1@users.noreply.github.com>
2022-12-24 16:44:35 +0530

    Update Main.java

--- src/main/java/com/bridgelabz/lambda/userregistration/Main.java
+++ src/main/java/com/bridgelabz/lambda/userregistration/Main.java
@@ -2,6 +2,6 @@ package com.bridgelabz.lambda.userregistration;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("welcome to User registration and Email Pattern Validation using Lambda!");
     }
 }
